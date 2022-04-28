6cd4182f7d8f74583b1c3415a1a5f47de0cb1269
Matheus.SBS <matheus.soares75@gmail.com>
2022-04-28 20:25:57 -0300

    form variables byId

---
+++ main.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
