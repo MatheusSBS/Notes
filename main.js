f8b0e6b4b18da2a4e3e5e31e2f6b0af81b6eb056
Matheus.SBS <matheus.soares75@gmail.com>
2022-04-28 20:29:10 -0300

    validate the input fild

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
+  });
+  
+  let formValidation = () => {
+    if (textInput.value === "") {
+      console.log("failure");
+      msg.innerHTML = "Task cannot be blank";
+    } else {
+      console.log("success");
+      msg.innerHTML = "";
+    }
+  };
