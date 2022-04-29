00e5990dd4052a7fa2e542553c10e78cae1a9ed0
Matheus.SBS <matheus.soares75@gmail.com>
2022-04-28 21:15:30 -0300

    function createTask and function resetForm

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ form.addEventListener("submit", (e) => {
       })();
     }
   };
-  
+
   let data = [];
 
 let acceptData = () => {
@@ -35,9 +35,34 @@ let acceptData = () => {
     text: textInput.value,
     date: dateInput.value,
     description: textarea.value,
+    createTasks();
   });
 
   localStorage.setItem("data", JSON.stringify(data));
 
   console.log(data);
 };
+let createTasks = () => {
+  tasks.innerHTML = "";
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+    <div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+    `);
+  });
+
+  resetForm();
+};
+let resetForm = () => {
+    textInput.value = "";
+    dateInput.value = "";
+    textarea.value = "";
+  };
