87aa17f0c3e18222a5886d0fdf24674ba7734860
Matheus.SBS <matheus.soares75@gmail.com>
2022-04-28 22:15:08 -0300

    function delet tasks on clink button

--- main.js
+++ main.js
@@ -67,3 +67,12 @@ let resetForm = () => {
     dateInput.value = "";
     textarea.value = "";
   };
+  let deleteTask = (e) => {
+    e.parentElement.parentElement.remove();
+  
+    data.splice(e.parentElement.parentElement.id, 1);
+  
+    localStorage.setItem("data", JSON.stringify(data));
+  
+    console.log(data);
+  };
