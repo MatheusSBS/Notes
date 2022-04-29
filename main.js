b32364cbdebac1931fc19c102bad594b504a242a
Matheus.SBS <matheus.soares75@gmail.com>
2022-04-28 22:17:24 -0300

    function edit task on click button

--- main.js
+++ main.js
@@ -76,3 +76,12 @@ let resetForm = () => {
   
     console.log(data);
   };
+  let editTask = (e) => {
+    let selectedTask = e.parentElement.parentElement;
+  
+    textInput.value = selectedTask.children[0].innerHTML;
+    dateInput.value = selectedTask.children[1].innerHTML;
+    textarea.value = selectedTask.children[2].innerHTML;
+  
+    deleteTask(e);
+  };
